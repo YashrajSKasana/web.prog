9d318ed22be3f631d1a4cd7585bbc85067273da7
YashrajSKasana <yashrjskasana2007@gmail.com>
2024-11-11 00:17:18 +0530

    removed the uncompletated contact page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./comp/Footer";
 import NavBar from "./comp/NavBar";
 const HomePage = lazy(() => import("./pages/HomePage.jsx"));
-const Contact = lazy(() => import("./pages/Contact.jsx"));
 const SerPage = lazy(() => import("./pages/SerPage.jsx"));
 const AboutPage = lazy(() => import("./pages/AboutPage.jsx"));
 const SerContPage = lazy(() => import("./pages/SerContPage.jsx"));
